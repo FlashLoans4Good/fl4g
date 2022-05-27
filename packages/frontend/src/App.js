9fe1b2969412019d2385757dab11e99e126cf105
Jon Liu <mr@sianliu.com>
2022-05-27 11:55:30 +0800

    Initial commit frontend and backend

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Application_Form from './components/application_form/Application_Form';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Application_Form />
+    </div>
+  );
+}
+
+export default App;
